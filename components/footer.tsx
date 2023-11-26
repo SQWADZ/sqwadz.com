57f870bd804fd5c0794c67c36c77894cdd3fe0d7
Luke <39926192+LukeWasTakenn@users.noreply.github.com>
2023-11-26 13:10:33 +0100

    refactor(footer): use class names rather than inline styles

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer: React.FC = () => {
   return (
-    <footer className="py-4 text-center" style={{ backgroundColor: 'var(--background)', color: 'var(--foreground)' }}>
+    <footer className="bg-background py-4 text-center text-muted-foreground">
       <p>© 2023 SQWADZ.com, All rights reserved.</p>
     </footer>
   );
