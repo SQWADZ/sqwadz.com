2392d293ddfd48c1fa11f4201575f61789686afb
Luke <39926192+LukeWasTakenn@users.noreply.github.com>
2023-11-26 13:08:52 +0100

    refactor: consistent component file naming

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,8 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import { ThemeProvider } from '@/components/theme-provider';
 import React from 'react';
-import Header from '@/components/Header';
-import Footer from '@/components/Footer';
+import Header from '@/components/header';
+import Footer from '@/components/footer';
 
 const inter = Inter({ subsets: ['latin'] });
 
