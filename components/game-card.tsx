e33ad53ae5f93fbc12eca1c93b8956cd2bda0642
Luke <39926192+LukeWasTakenn@users.noreply.github.com>
2023-12-03 12:31:12 +0100

    feat(game-card): half assed twitch-like hover effect

--- components/game-card.tsx
+++ components/game-card.tsx
@@ -13,10 +13,18 @@ const GameCard: React.FC<Props> = ({ name, image, path }) => {
 
   return (
     <Link href={gamePageUrl}>
-      <div className="flex cursor-pointer items-center justify-start gap-8">
-        <div className="flex flex-col items-center justify-center gap-2">
-          <Image src={image} width={400} height={150} alt={name} className="h-[150px] rounded-lg" />
-          <p>{name}</p>
+      <div className="group flex cursor-pointer items-center justify-start gap-8">
+        <div className="flex flex-col items-start justify-center gap-2">
+          <div className="rounded-lg bg-transparent transition-colors duration-150 group-hover:bg-destructive">
+            <Image
+              src={image}
+              width={400}
+              height={150}
+              alt={name}
+              className="h-[150px] rounded-lg duration-150 group-hover:-translate-y-1 group-hover:translate-x-1 group-hover:transition-all"
+            />
+          </div>
+          <p className="line-clamp-1">{name}</p>
         </div>
       </div>
     </Link>
