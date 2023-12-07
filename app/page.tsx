d21e5f326c6a3db5d20686e59737ba525d9bea61
Luke <39926192+LukeWasTakenn@users.noreply.github.com>
2023-12-07 12:41:25 +0100

    feat: hero on root page

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,38 @@
-import GameCard from '@/components/game-card';
-import games from '@/data/games.json';
+import React from 'react';
+import Container from '@/components/container';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faS } from '@fortawesome/free-solid-svg-icons';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import { getServerAuthSession } from '@/server/auth';
 
-export default function Home() {
+const HeroPage: React.FC = async () => {
+  const session = await getServerAuthSession();
+
   return (
-    <div className="mx-auto flex max-w-7xl flex-1 flex-col gap-8 p-4">
-      <div className="flex flex-col gap-0">
-        <p className="text-xl">Games</p>
-        <p className="sm text-muted-foreground">Select a game to find a group in</p>
+    <Container className="flex flex-1 flex-col-reverse items-center justify-center gap-12 md:flex-row">
+      <div className="flex flex-[0.5] flex-col items-center gap-8 md:items-start">
+        <div className="flex flex-col items-center gap-2 md:items-start">
+          <p className="text-center text-4xl md:text-left">
+            Finding groups has <span className="font-bold text-primary">never</span> been easier
+          </p>
+          <p className="text-sm text-muted-foreground">Browse games, select a room, join the group.</p>
+        </div>
+        <div className="flex gap-4">
+          <Link href="/games">
+            <Button variant="secondary">Browse games</Button>
+          </Link>
+          <Link href={session?.user ? '/games' : '/sign-in'}>
+            <Button>Sign in</Button>
+          </Link>
+        </div>
       </div>
-
-      <div className="grid grid-cols-1 place-items-center gap-8 md:grid-cols-3">
-        {games.map((game) => (
-          <GameCard key={game.name} name={game.name} image={game.image} path={game.path} color={game.color} />
-        ))}
+      <div className="flex flex-[0.5] items-center md:justify-end">
+        {/*SQWADZ logo here*/}
+        <FontAwesomeIcon icon={faS} size="10x" fixedWidth />
       </div>
-    </div>
+    </Container>
   );
-}
+};
+
+export default HeroPage;
