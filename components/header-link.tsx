d3c2b88e94fa3989b8fb969f3661b3aea1449072
Luke <39926192+LukeWasTakenn@users.noreply.github.com>
2023-12-08 22:38:17 +0100

    style(header-link): don't highlight link unless path matches pathname

--- components/header-link.tsx
+++ components/header-link.tsx
@@ -8,13 +8,11 @@ import { cn } from '@/lib/utils';
 const HeaderLink: React.FC<{ path: string; label: string }> = ({ path, label }) => {
   const pathname = usePathname();
 
-  console.log(pathname);
-
   return (
     <div
       className={cn(
         'flex gap-2 text-muted-foreground hover:text-secondary-foreground',
-        pathname.includes(path) && 'text-secondary-foreground'
+        pathname === path && 'text-secondary-foreground'
       )}
     >
       <Link href={path}>{label}</Link>
