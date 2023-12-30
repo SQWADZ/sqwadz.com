88b976ce78867ec8e00354d99756b8385e3ee8c8
Chris Judd <85649796+juddisjudd@users.noreply.github.com>
2023-12-29 23:26:22 -0800

    add: support-us page w/ Buy Me a Coffee button

--- app/support-us/page.tsx
+++ app/support-us/page.tsx
@@ -2,7 +2,15 @@ import Container from '@/components/container';
 import React from 'react';
 
 const SupportUsPage: React.FC = () => {
-  return <Container>TODO</Container>;
+  return (
+    <Container>
+      <div className="flex flex-col items-center justify-center h-full">
+        <a href="https://www.buymeacoffee.com/sqwadz">
+          <img src="https://img.buymeacoffee.com/button-api/?text=Buy us a coffee&emoji=&slug=sqwadz&button_colour=FFDD00&font_colour=000000&font_family=Inter&outline_colour=000000&coffee_colour=ffffff" />
+        </a>
+      </div>
+    </Container>
+  );
 };
 
 export default SupportUsPage;
