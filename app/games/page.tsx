2231cde669ac587bdbd01174d37549c121166aa2
Luke <39926192+LukeWasTakenn@users.noreply.github.com>
2023-12-31 15:29:34 +0100

    refactor(games): use container component

--- app/games/page.tsx
+++ app/games/page.tsx
@@ -1,9 +1,10 @@
+import Container from '@/components/container';
 import GameCard from '@/components/game-card';
 import games from '@/data/games.json';
 
 export default function Home() {
   return (
-    <div className="mx-auto flex max-w-7xl flex-1 flex-col gap-8 p-4">
+    <Container className="flex max-w-7xl flex-col gap-8">
       <div className="flex flex-col gap-0">
         <p className="text-xl">Games</p>
         <p className="sm text-muted-foreground">Select a game to find a group in</p>
@@ -14,6 +15,6 @@ export default function Home() {
           <GameCard key={game.name} name={game.name} image={game.image} path={game.path} color={game.color} />
         ))}
       </div>
-    </div>
+    </Container>
   );
 }
