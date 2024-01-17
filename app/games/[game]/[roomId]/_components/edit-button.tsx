c6c22ddb2db45a7681db09b19e6cc689e4051225
Luke <39926192+LukeWasTakenn@users.noreply.github.com>
2024-01-17 10:39:10 +0100

    feat: ability to edit room settings

---
+++ app/games/[game]/[roomId]/_components/edit-button.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import React from 'react';
+import { Button } from '@/components/ui/button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGear } from '@fortawesome/free-solid-svg-icons';
+import { useModal } from '@/components/modals-provider';
+import RoomSettingsModal from '@/app/games/[game]/[roomId]/_components/room-settings-modal';
+
+interface Props {
+  activity: string;
+  slots: number;
+  roomId: number;
+}
+
+const EditButton: React.FC<Props> = ({ activity, slots, roomId }) => {
+  const modal = useModal();
+
+  return (
+    <Button
+      size="icon"
+      variant="secondary"
+      onClick={() =>
+        modal.open({
+          title: 'Settings',
+          children: <RoomSettingsModal roomId={roomId} activity={activity} slots={slots} />,
+        })
+      }
+    >
+      <FontAwesomeIcon icon={faGear} fixedWidth size="lg" />
+    </Button>
+  );
+};
+
+export default EditButton;
