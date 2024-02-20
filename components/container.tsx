7107c6424601886db97738287451910047b6439f
Luke <39926192+LukeWasTakenn@users.noreply.github.com>
2024-02-20 20:52:26 +0100

    style: add min height for main container
    resolves headaches around having scrollbars on some pages and not others causing content shift

--- components/container.tsx
+++ components/container.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { cn } from '@/lib/utils';
 
 const Container: React.FC<{ children: React.ReactNode; className?: string }> = ({ children, className }) => {
-  return <div className={cn('mx-auto w-full max-w-7xl flex-1 p-4', className)}>{children}</div>;
+  return <div className={cn('mx-auto min-h-screen w-full max-w-7xl flex-1 p-4', className)}>{children}</div>;
 };
 
 export default Container;
