5d8fde11e4a68a4bc0e642db0f6dc46f5bd576bb
Chris Judd <85649796+juddisjudd@users.noreply.github.com>
2024-03-11 02:31:33 -0700

    fix: quote things

--- app/privacy-policy/page.tsx
+++ app/privacy-policy/page.tsx
@@ -4,75 +4,66 @@ import React from 'react';
 const PrivacyPolicyPage: React.FC = () => {
   return (
     <Container>
-      <h1 className="text-3xl font-bold mb-4">Privacy Policy</h1>
+      <h1 className="mb-4 text-3xl font-bold">Privacy Policy</h1>
       <div className="mb-4">
         <h2 className="text-xl font-bold">1. Introduction</h2>
         <p>
-          At Sqwadz, we value the privacy of our users and are committed to
-          protecting your personal information. This Privacy Policy outlines how
-          we collect, use, and safeguard the information you provide when using
-          our website (https://sqwadz.com/) and services.
+          At Sqwadz, we value the privacy of our users and are committed to protecting your personal information. This
+          Privacy Policy outlines how we collect, use, and safeguard the information you provide when using our website
+          (https://sqwadz.com/) and services.
         </p>
       </div>
       <div className="mb-4">
         <h2 className="text-xl font-bold">2. Information We Collect</h2>
         <p>
-          We may collect personal information such as your name, email address,
-          and other contact details when you create an account or interact with
-          our services. We may also collect non-personal information such as
-          your device information, browsing data, and usage statistics.
+          We may collect personal information such as your name, email address, and other contact details when you
+          create an account or interact with our services. We may also collect non-personal information such as your
+          device information, browsing data, and usage statistics.
         </p>
       </div>
       <div className="mb-4">
         <h2 className="text-xl font-bold">3. Use of Information</h2>
         <p>
-          We use the information we collect to provide, maintain, and improve
-          our services, as well as for communication purposes, analytics, and
-          personalization of your experience on our website.
+          We use the information we collect to provide, maintain, and improve our services, as well as for communication
+          purposes, analytics, and personalization of your experience on our website.
         </p>
       </div>
       <div className="mb-4">
         <h2 className="text-xl font-bold">4. Data Sharing and Disclosure</h2>
         <p>
-          We do not sell or rent your personal information to third parties. We
-          may share your information with trusted service providers who assist us
-          in operating our website and delivering our services. We may also
-          disclose your information as required by law or to protect our rights
-          and interests.
+          We do not sell or rent your personal information to third parties. We may share your information with trusted
+          service providers who assist us in operating our website and delivering our services. We may also disclose
+          your information as required by law or to protect our rights and interests.
         </p>
       </div>
       <div className="mb-4">
         <h2 className="text-xl font-bold">5. Data Security</h2>
         <p>
-          We implement reasonable security measures to protect your personal
-          information from unauthorized access, disclosure, or misuse. However,
-          no method of transmission or storage is completely secure, and we
-          cannot guarantee absolute security.
+          We implement reasonable security measures to protect your personal information from unauthorized access,
+          disclosure, or misuse. However, no method of transmission or storage is completely secure, and we cannot
+          guarantee absolute security.
         </p>
       </div>
       <div className="mb-4">
         <h2 className="text-xl font-bold">6. Third-Party Links</h2>
         <p>
-          Our website may contain links to third-party websites or services that
-          are not governed by this Privacy Policy. We encourage you to review
-          the privacy policies of those third parties before providing any
-          personal information.
+          Our website may contain links to third-party websites or services that are not governed by this Privacy
+          Policy. We encourage you to review the privacy policies of those third parties before providing any personal
+          information.
         </p>
       </div>
       <div className="mb-4">
-        <h2 className="text-xl font-bold">7. Children's Privacy</h2>
+        <h2 className="text-xl font-bold">7. Children&#39;s Privacy</h2>
         <p>
-          Our services are not intended for children under the age of 13. We do
-          not knowingly collect personal information from children without
-          parental consent.
+          Our services are not intended for children under the age of 13. We do not knowingly collect personal
+          information from children without parental consent.
         </p>
       </div>
       <div className="mb-4">
         <h2 className="text-xl font-bold">8. Changes to this Privacy Policy</h2>
         <p>
-          We may update this Privacy Policy from time to time to reflect changes
-          in our practices or legal requirements. We encourage you to review this
-          page periodically for the latest information.
+          We may update this Privacy Policy from time to time to reflect changes in our practices or legal requirements.
+          We encourage you to review this page periodically for the latest information.
         </p>
       </div>
     </Container>
--- app/terms-of-service/page.tsx
+++ app/terms-of-service/page.tsx
@@ -4,64 +4,54 @@ import Container from '@/components/container';
 const TermsOfServicePage: React.FC = () => {
   return (
     <Container>
-      <h1 className="text-3xl font-bold mb-4">Sqwadz Terms of Service</h1>
+      <h1 className="mb-4 text-3xl font-bold">Sqwadz Terms of Service</h1>
       <div className="mb-4">
         <h2 className="text-xl font-bold">1. Introduction</h2>
         <p>
-          Welcome to Sqwadz, a platform for managing and coordinating teams and
-          group activities. By accessing or using https://sqwadz.com/ (the
-          "Website"), you agree to be bound by these Terms of Service and our
-          Privacy Policy.
+          Welcome to Sqwadz, a platform for managing and coordinating teams and group activities. By accessing or using
+          https://sqwadz.com/ (the &quot;Website&quot;), you agree to be bound by these Terms of Service and our Privacy
+          Policy.
         </p>
       </div>
       <div className="mb-4">
         <h2 className="text-xl font-bold">2. Use of Services</h2>
         <p>
-          You agree to use the Website and its services only for lawful purposes
-          and in accordance with these Terms of Service. You are responsible for
-          your use of the Website and any consequences arising from such use.
+          You agree to use the Website and its services only for lawful purposes and in accordance with these Terms of
+          Service. You are responsible for your use of the Website and any consequences arising from such use.
         </p>
       </div>
       <div className="mb-4">
         <h2 className="text-xl font-bold">3. Intellectual Property</h2>
         <p>
-          The Website and its content are protected by intellectual property
-          laws. You may not use or reproduce our content without our prior
-          written consent.
+          The Website and its content are protected by intellectual property laws. You may not use or reproduce our
+          content without our prior written consent.
         </p>
       </div>
       <div className="mb-4">
         <h2 className="text-xl font-bold">4. User Accounts</h2>
         <p>
-          You may be required to create an account to access certain features of
-          the Website. You are responsible for maintaining the confidentiality
-          of your account credentials and for any activities that occur under
-          your account.
+          You may be required to create an account to access certain features of the Website. You are responsible for
+          maintaining the confidentiality of your account credentials and for any activities that occur under your
+          account.
         </p>
       </div>
       <div className="mb-4">
         <h2 className="text-xl font-bold">5. Disclaimer of Warranties</h2>
         <p>
-          We provide the Website and its services "as is" without any
-          warranties or representations, express or implied. We do not guarantee
-          the accuracy, reliability, or completeness of the Website or its
-          content.
+          We provide the Website and its services &quot;as is&quot; without any warranties or representations, express
+          or implied. We do not guarantee the accuracy, reliability, or completeness of the Website or its content.
         </p>
       </div>
       <div className="mb-4">
         <h2 className="text-xl font-bold">6. Limitation of Liability</h2>
         <p>
-          In no event shall we be liable for any indirect, incidental, special,
-          or consequential damages arising from your use of the Website or its
-          services.
+          In no event shall we be liable for any indirect, incidental, special, or consequential damages arising from
+          your use of the Website or its services.
         </p>
       </div>
       <div className="mb-4">
         <h2 className="text-xl font-bold">7. Modifications</h2>
-        <p>
-          We reserve the right to modify or terminate these Terms of Service at
-          any time without prior notice.
-        </p>
+        <p>We reserve the right to modify or terminate these Terms of Service at any time without prior notice.</p>
       </div>
     </Container>
   );
