2a950f6603ec5c94bfbf44978f705b491d8b17e6
Luke <39926192+LukeWasTakenn@users.noreply.github.com>
2024-04-01 18:29:35 +0200

    style(footer): move copyright to the left side

--- components/footer.tsx
+++ components/footer.tsx
@@ -8,41 +8,43 @@ const Footer: React.FC = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="m-auto flex w-full flex-col items-center justify-between border-t bg-background p-4 text-center text-muted-foreground">
-      <div className="flex w-full max-w-7xl flex-col items-start justify-between gap-10 py-10 md:flex-row">
-        <Link href="/" className="self-center">
-          <p className="logo text-4xl text-foreground">SQWADZ</p>
-        </Link>
-        <div className="flex flex-col items-baseline justify-start gap-4">
-          <p className="text-sm">Terms & Privacy</p>
-          <Link href="/privacy-policy" className="hover:text-foreground">
-            Privacy Policy
+    <footer className="flex w-full flex-col items-center justify-between border-t bg-background p-4 text-center text-muted-foreground">
+      <div className="flex w-full max-w-7xl flex-col">
+        <div className="flex w-full max-w-7xl flex-col items-start justify-between gap-10 py-10 md:flex-row">
+          <Link href="/" className="self-center">
+            <p className="logo text-4xl text-foreground">SQWADZ</p>
           </Link>
-          <Link href="/terms-of-service" className="hover:text-foreground">
-            Terms of Service
-          </Link>
+          <div className="flex flex-col items-baseline justify-start gap-4">
+            <p className="text-sm">Terms & Privacy</p>
+            <Link href="/privacy-policy" className="hover:text-foreground">
+              Privacy Policy
+            </Link>
+            <Link href="/terms-of-service" className="hover:text-foreground">
+              Terms of Service
+            </Link>
+          </div>
+          <div className="flex flex-col items-baseline gap-4">
+            <p className="text-sm">Socials</p>
+            <Link
+              href="https://twitter.com/sqwadzplatform"
+              target="_blank"
+              className="flex items-center gap-2 hover:text-foreground"
+            >
+              <FontAwesomeIcon icon={faTwitter} fixedWidth />
+              <p>Twitter</p>
+            </Link>
+            <Link
+              href="https://discord.sqwadz.com"
+              target="_blank"
+              className="flex items-center gap-2 hover:text-foreground"
+            >
+              <FontAwesomeIcon icon={faDiscord} fixedWidth />
+              <p>Discord</p>
+            </Link>
+          </div>
         </div>
-        <div className="flex flex-col items-baseline gap-4">
-          <p className="text-sm">Socials</p>
-          <Link
-            href="https://twitter.com/sqwadzplatform"
-            target="_blank"
-            className="flex items-center gap-2 hover:text-foreground"
-          >
-            <FontAwesomeIcon icon={faTwitter} fixedWidth />
-            <p>Twitter</p>
-          </Link>
-          <Link
-            href="https://discord.sqwadz.com"
-            target="_blank"
-            className="flex items-center gap-2 hover:text-foreground"
-          >
-            <FontAwesomeIcon icon={faDiscord} fixedWidth />
-            <p>Discord</p>
-          </Link>
-        </div>
+        <p className="mt-8 self-start text-sm">© {currentYear} SQWADZ.com, All rights reserved.</p>
       </div>
-      <p className="mt-8">© {currentYear} SQWADZ.com, All rights reserved.</p>
     </footer>
   );
 };
