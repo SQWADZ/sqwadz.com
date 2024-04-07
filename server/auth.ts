76df73328fc53a71f5794d3ac8d03aed0341f7ac
Luke <39926192+LukeWasTakenn@users.noreply.github.com>
2024-04-07 23:08:42 +0200

    feat: epic games auth

--- server/auth.ts
+++ server/auth.ts
@@ -56,7 +56,44 @@ export const authOptions: NextAuthOptions = {
     // into a `sub` column, which does not exist in our schema so we have to use this hacky workaround
 
     // @ts-ignore
-    linkAccount: ({ sub, ...data }) => prisma.account.create({ data }),
+    linkAccount: ({ sub, ...data }) => {
+      if (data.expires_at && typeof data.expires_at === 'string') {
+        data.expires_at = new Date(data.expires_at).valueOf() / 1000;
+      }
+
+      // Some providers such as epic send additional data that is not relevant to us and causes
+      // and error in prisma as it tries to insert it into the column, so we destruct the whole data object
+
+      const {
+        userId,
+        type,
+        provider,
+        providerAccountId,
+        refresh_token,
+        access_token,
+        expires_at,
+        token_type,
+        scope,
+        id_token,
+        session_state,
+      } = data;
+
+      return prisma.account.create({
+        data: {
+          userId,
+          type,
+          provider,
+          providerAccountId,
+          refresh_token,
+          access_token,
+          expires_at,
+          token_type,
+          scope,
+          id_token,
+          session_state,
+        },
+      });
+    },
   },
   providers: [
     DiscordProvider({
@@ -73,6 +110,50 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.BNET_CLIENT_SECRET as string,
       issuer: process.env.BNET_ISSUER as BattleNetIssuer,
     }),
+    {
+      id: 'epic',
+      name: 'Epic',
+      type: 'oauth',
+      wellKnown: 'https://api.epicgames.dev/epic/oauth/v1/.well-known/openid-configuration',
+      clientId: process.env.EPIC_CLIENT_ID,
+      clientSecret: process.env.EPIC_CLIENT_SECRET,
+      authorization: { params: { scope: 'openid basic_profile' } },
+      token: {
+        async request(context) {
+          const authToken = Buffer.from(`${process.env.EPIC_CLIENT_ID}:${process.env.EPIC_CLIENT_SECRET}`).toString(
+            'base64'
+          );
+
+          const params = new URLSearchParams();
+
+          params.append('grant_type', 'authorization_code');
+          params.append('code', context.params.code as string);
+          params.append('scope', 'openid basic_profile');
+          params.append('state', context.params.state as string);
+
+          const response = await fetch('https://api.epicgames.dev/epic/oauth/v2/token', {
+            method: 'POST',
+            body: params,
+            headers: {
+              'Content-Type': 'application/x-www-form-urlencoded',
+              Authorization: `Basic ${authToken}`,
+            },
+          });
+
+          const tokens = await response.json();
+
+          return { tokens };
+        },
+      },
+      profile(profile) {
+        return {
+          id: profile.sub,
+          name: profile.dn,
+          email: null,
+          image: null,
+        };
+      },
+    },
   ],
   pages: {
     signIn: '/sign-in',
