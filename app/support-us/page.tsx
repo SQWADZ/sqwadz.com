10114133edae8dfc021f72d23e27b875cab588aa
Chris Judd <85649796+juddisjudd@users.noreply.github.com>
2024-04-15 08:22:30 -0700

    update: removed iframe ko-fi button for tailwind button

--- app/support-us/page.tsx
+++ app/support-us/page.tsx
@@ -1,21 +1,19 @@
 import Container from '@/components/container';
 import React from 'react';
+import Link from 'next/link';
 
 const SupportUsPage: React.FC = () => {
   return (
-    <Container className="flex flex-col gap-8">
-      <div className="flex flex-col gap-0">
-        <p className="text-xl">Support us</p>
-        <p className="sm text-muted-foreground">Enjoy what we made? Consider supporting us!</p>
+    <Container className="flex flex-col items-center justify-center p-10 gap-8">
+      <div className="text-center">
+        <p className="text-xl text-gray-600">Enjoy what we made? Consider supporting us!</p>
       </div>
-      <div className="flex h-full flex-col items-center justify-center">
-        <iframe 
-          id='kofiframe' 
-          src='https://ko-fi.com/sqwadz/?hidefeed=true&widget=true&embed=true&preview=true'
-          style={{ width: '21vw', height: '66vh', backgroundColor: 'transparent' }}
-          title='sqwadz'
-        />
-      </div>
+      <Link href="https://ko-fi.com/sqwadz" 
+            target="_blank" 
+            rel="noopener noreferrer" 
+            className="bg-red-500 hover:bg-red-600 text-white font-semibold text-lg px-6 py-3 rounded-lg shadow transition duration-300 ease-in-out transform hover:-translate-y-1">
+        Support on Ko-fi
+      </Link>
     </Container>
   );
 };
