ecf0ee78b778a1cb217143a924a7ed446875853c
Chris Judd <85649796+juddisjudd@users.noreply.github.com>
2024-05-25 23:21:33 -0700

    feat: Update game room listing to display game name dynamically

--- app/games/[game]/page.tsx
+++ app/games/[game]/page.tsx
@@ -5,6 +5,7 @@ import { getServerAuthSession } from '@/server/auth';
 import RoomSearch from '@/app/games/[game]/_components/room-search';
 import Rooms from '@/app/games/[game]/_components/table/rooms';
 import TableLoadingSkeleton from '@/app/games/[game]/_components/table/table-loading-skeleton';
+import games from '@/data/games.json';
 
 const GamePage: React.FC<{ params: { game: string }; searchParams: { query?: string; page?: string } }> = async ({
   params,
@@ -13,10 +14,12 @@ const GamePage: React.FC<{ params: { game: string }; searchParams: { query?: str
   const session = await getServerAuthSession();
   const _page = searchParams.page ? +searchParams.page : undefined;
 
+  const gameDetails = games.find((game) => game.path === params.game);
+
   return (
     <Container className="flex flex-col gap-10">
       <div className="flex items-center justify-between">
-        <p className="text-xl">Rooms</p>
+      <p className="text-xl">{gameDetails ? gameDetails.name : 'Rooms'}</p>
         <CreateRoom session={session} game={params.game} />
       </div>
       <RoomSearch />
