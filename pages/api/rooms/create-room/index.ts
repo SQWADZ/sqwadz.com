6b5d1418b95fced3f5ee188ddfdfe00c587c5c71
Chris Judd <85649796+juddisjudd@users.noreply.github.com>
2024-05-29 00:50:19 -0700

    chore: Add profanity filter to room creation endpoint

--- pages/api/rooms/create-room/index.ts
+++ pages/api/rooms/create-room/index.ts
@@ -3,6 +3,7 @@ import { NextApiResponseServerIo } from '@/pages/api/socket/io';
 import { getPagesServerAuthSession, getServerAuthSession } from '@/server/auth';
 import prisma from '@/lib/prisma';
 import { roomRemovalQueue } from '@/lib/bullmq';
+import Filter from 'bad-words';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponseServerIo) {
   const session = await getPagesServerAuthSession(req, res);
@@ -12,6 +13,11 @@ export default async function handler(req: NextApiRequest, res: NextApiResponseS
   const data: { activity: string; password?: string; slots: number; game: string } = req.body;
 
   if (data.activity.length > 50) return res.status(401).json({ error: 'Activity name too long' });
+
+  const filter = new Filter();
+  if (filter.isProfane(data.activity)) {
+    return res.status(400).json({ error: 'Activity name contains inappropriate language' });
+  }
 
   const { id } = await prisma.room.create({
     data: {
