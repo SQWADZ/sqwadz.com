c030062deb5a4021509d896b0cefc287146554f2
Chris Judd <85649796+juddisjudd@users.noreply.github.com>
2024-06-01 23:28:03 -0700

    chore: Update metadata for SEO and Open Graph

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,20 +32,18 @@ export const metadata: Metadata = {
       },
     ],
   },
-  alternates: {
-    canonical: '/',
-  },
   robots: {
     index: true,
     follow: true,
   },
   other: {
     'application/ld+json': JSON.stringify({
-      "@context": "https://schema.org",
-      "@type": "WebPage",
-      "name": "Finding Groups Made Easy",
-      "description": 'Browse games, select a room, and join the group effortlessly. Finding groups has never been easier.',
-      "url": 'https://sqwadz.com/',
+      '@context': 'https://schema.org',
+      '@type': 'WebPage',
+      name: 'Finding Groups Made Easy',
+      description:
+        'Browse games, select a room, and join the group effortlessly. Finding groups has never been easier.',
+      url: 'https://sqwadz.com/',
     }),
   },
 };
