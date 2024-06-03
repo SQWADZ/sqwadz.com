250da320a32729807a34c6aa4a10396a3f282fe2
Chris Judd <85649796+juddisjudd@users.noreply.github.com>
2024-06-03 09:07:45 -0700

    chore: revert changes for CSP in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,42 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async headers() {
-    return [
+  images: {
+    remotePatterns: [
       {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: `
-              default-src 'self'; 
-              img-src 'self' data:; 
-              media-src 'self'; 
-              script-src 'self' 'unsafe-inline' 'unsafe-eval' https://umami.bawkbawk.net; 
-              style-src 'self' 'unsafe-inline'; 
-              frame-src 'self'; 
-              font-src 'self'; 
-              connect-src 'self' https://umami.bawkbawk.net https://db.sqwadz.com https://us.battle.net/oauth https://id.twitch.tv https://api.epicgames.dev https://discord.com; 
-              base-uri 'self'; 
-              form-action 'self'; 
-              frame-ancestors 'self'; 
-              child-src 'self'; 
-              manifest-src 'self'; 
-              object-src 'none'; 
-              block-all-mixed-content; 
-              upgrade-insecure-requests;
-            `.replace(/\s{2,}/g, ' ').trim(),
-          },
-          {
-            key: 'Referrer-Policy',
-            value: 'strict-origin-when-cross-origin',
-          },
-          {
-            key: 'X-Frame-Options',
-            value: 'SAMEORIGIN',
-          },
-        ],
+        protocol: 'https',
+        hostname: 'cdn.discordapp.com',
+        port: '',
+        pathname: '/attachments/**',
       },
-    ];
+    ],
   },
 };
 
