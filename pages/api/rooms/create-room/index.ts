0cfacc358153d2f27832e4bd901c9e2ad6604b34
Luke <39926192+LukeWasTakenn@users.noreply.github.com>
2024-07-04 19:48:12 +0200

    fix(rooms): send isVerified for room creator to socket event

--- pages/api/rooms/create-room/index.ts
+++ pages/api/rooms/create-room/index.ts
@@ -57,6 +57,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponseS
       creator: {
         select: {
           name: true,
+          isVerified: true,
         },
       },
       _count: {
