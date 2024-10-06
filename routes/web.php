73b29b9ff12d057862976dfa77b03fc33dd7936d
Luke <39926192+LukeWasTakenn@users.noreply.github.com>
2024-10-06 11:47:02 +0200

    style: red theme

--- routes/web.php
+++ routes/web.php
@@ -5,13 +5,6 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-})->name('/');
+Route::get('/', fn () => Inertia::render("Welcome"))->name('/');
 
 require __DIR__.'/auth.php';
